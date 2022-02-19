b8d5926d692426a70282daaf780d19191e836cf8
Leonardo Sena <lgomesgabriel@hotmail.com>
2022-02-19 17:15:35 -0300

    Entity Course

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://cdn.pixabay.com/photo/2021/03/18/17/09/teacher-6105336_1280.png', 'https://w1.pngwing.com/pngs/771/463/png-transparent-school-black-and-white-student-class-course-education-teacher-school-logo.png');
